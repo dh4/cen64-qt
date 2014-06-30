72af1796482a49307c8bbc06b5e78612a9b093cd
Dan Hasting <dan@hasting.info>
2014-06-30 15:07:03 -0400

    Fix issue with window position

--- src/cen64qt.cpp
+++ src/cen64qt.cpp
@@ -304,10 +304,10 @@ void CEN64Qt::checkStatus(int status)
 
 void CEN64Qt::closeEvent(QCloseEvent *event)
 {
-    SETTINGS.setValue("Geometry/windowx", x());
-    SETTINGS.setValue("Geometry/windowy", y());
-    SETTINGS.setValue("Geometry/width", width());
-    SETTINGS.setValue("Geometry/height", height());
+    SETTINGS.setValue("Geometry/windowx", geometry().x());
+    SETTINGS.setValue("Geometry/windowy", geometry().y());
+    SETTINGS.setValue("Geometry/width", geometry().width());
+    SETTINGS.setValue("Geometry/height", geometry().height());
     if (isMaximized())
         SETTINGS.setValue("Geometry/maximized", true);
     else
