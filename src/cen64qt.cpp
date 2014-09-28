fbb85ab7ace6e03e49e26aec945988d49d49b7fe
Dan Hasting <dan@hasting.info>
2014-09-28 13:10:46 -0400

    Small fix

--- src/cen64qt.cpp
+++ src/cen64qt.cpp
@@ -179,7 +179,6 @@ void CEN64Qt::addRoms()
                     progress->setValue(count);
                 }
 
-                database.close();
                 progress->close();
             } else {
             QMessageBox::warning(this, "Warning", "No ROMs found.");
@@ -211,6 +210,8 @@ void CEN64Qt::addRoms()
         gridView->setEnabled(true);
         listView->setEnabled(true);
     }
+
+    database.close();
 }
 
 
